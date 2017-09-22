f52cc8426845bf0915b40ec4a870f6c3614f66a0
Bruno Costa <bruno.henrique@kabum.com.br>
2017-09-22 17:12:41 -0300

    Python e Phalcon

---
+++ phalcon/www/app/controllers/UsersController.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class UsersController extends Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+    public function signinAction()
+    {
+
+    }
+
+}
