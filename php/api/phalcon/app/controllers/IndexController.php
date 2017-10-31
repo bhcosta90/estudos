5c0545e35bdda28a4ddf7f0afff2ed4c8e9d6432
Bruno Costa <bruno.henrique@kabum.com.br>
2017-10-31 09:34:03 -0200

    Colocando Phalcon no Ar

---
+++ php/api/phalcon/app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        
+    }
+
+    public function registerAction()
+    {
+        
+    }
+}
