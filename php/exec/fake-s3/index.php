6556b5a3e254f2bd34b3f6795d63423017db8c0d
Bruno Costa <bruno.costa@superlogica.com>
2020-12-08 08:13:40 -0300

    arrumando fake s3

--- php/exec/fake-s3/index.php
+++ php/exec/fake-s3/index.php
@@ -2,13 +2,13 @@
 include "vendor/autoload.php";
 
 use Aws\S3\S3Client;
-use Aws\Credentials\Credentials;
+// use Aws\Credentials\Credentials;
 use Aws\S3\Exception\S3Exception;
 
-$s3 = S3Client::factory(array(
+$s3 = new S3Client(array(
     'version' => 'latest',
-    'region'  => '',
-    'endpoint' => 'http://fakes3:4567',
+    'region'  => 'us-east-1',
+    'endpoint' => 'http://fakes3:4569',
     'credentials' => [
         'key' => '',
         'secret' => ''
@@ -23,33 +23,30 @@ $size = getimagesize(__DIR__ . "/img/84179_index_gg.jpg");
 
 $key = 123456;
 
-/*$s3->putObject(array(
-    'Bucket'       => "s3",
-    'Key'          => $key . ".jpg",
-    'SourceFile'   => __DIR__ . "/img/84179_index_gg.jpg",
-    'ContentType'  => $size['mime'],
-    'ACL'          => 'public-read',
-    'StorageClass' => 'REDUCED_REDUNDANCY'
-));
+// $s3->putObject(array(
+//     'Bucket'       => "s3",
+//     'Key'          => $key . ".jpg",
+//     'SourceFile'   => __DIR__ . "/img/84179_index_gg.jpg",
+//     'ContentType'  => $size['mime'],
+//     'ACL'          => 'public-read',
+//     'StorageClass' => 'REDUCED_REDUNDANCY'
+// ));
 
-print "<img src='http://localhost:4567/fakes3/s3/$key.jpg' />";//*/
+print "<img src='http://localhost:4569/fakes3/s3/$key.jpg' />";//*/
 
 try {
-    $objects = $s3->getIterator('ListObjects', array(
+    $results = $s3->getPaginator('ListObjects', [
         'Bucket' => 's3'
-    ));
+    ]);
 
-    echo "Keys retrieved!\n";
-    print "<pre>";
-    print_r($objects);
-    // foreach ($objects as $object) {
-    //     echo "<br>";
-    //     print_r($object);
-    // }
-    print "</pre>";
+    foreach ($results as $result) {
+        foreach ($result['Contents'] as $object) {
+            echo $object['Key'] . PHP_EOL;
+        }
+    }
 } catch (S3Exception $e) {
-    echo $e->getMessage() . "\n";
-}//*/
+    echo $e->getMessage() . PHP_EOL;
+}
 
 /*$s3->deleteObject(array(
     'Bucket' => "s3",
